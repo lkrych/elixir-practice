e1c9b7879746f21accfba66225e801a6f9ed76db
Leland Krych <Leland.Krych@gmail.com>
2017-10-07 09:40:47 -0700

    create shuffle method for Cards module

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,10 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def create_deck do
+    ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
   end
+
+  def shuffle(deck) do 
+    Enum.shuffle(deck)
+  end
+  
 end
