41a3f68ba9d5da35106330f6a90e449a5c99f40d
Leland Krych <Leland.Krych@gmail.com>
2017-10-07 22:45:21 -0700

    create grid for eventual display in Identicon module

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -9,11 +9,7 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
-  end
-
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b | _tail ]} = image
-    [r, g, b]
+    |> build_grid
   end
 
   def hash_input(input) do
@@ -22,4 +18,28 @@ defmodule Identicon do
 
     %Identicon.Image{hex: hex}
   end
+
+  #pattern match off of argument list!
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail ]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  # you need to pass a reference to mirror_row,
+  # passing mirror_row without an ampersand would immediately invoke the function
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid = 
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten 
+      |> Enum.with_index
+
+    %Identicon.Image{ image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+
 end
