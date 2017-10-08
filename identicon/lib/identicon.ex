8dd779d94c3fbb6f08b473780df508b03b289573
Leland Krych <Leland.Krych@gmail.com>
2017-10-07 22:12:20 -0700

    create pick_color, hash_input and main functions for Identicon.

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,6 +1,25 @@
 defmodule Identicon do
   @moduledoc """
   Documentation for Identicon.
-  """
 
+  Create a unique identicon for any given string.
+  
+  """
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: [r, g, b | _tail ]} = image
+    [r, g, b]
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
 end
