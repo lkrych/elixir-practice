a1194a7ce986326e249eb21528ea68a21f25de34
Leland Krych <Leland.Krych@gmail.com>
2017-10-16 09:05:37 -0700

    add private method for signin

--- discuss/web/models/user.ex
+++ discuss/web/models/user.ex
@@ -2,7 +2,7 @@ defmodule Discuss.User do
   use Discuss.Web, :model
 
   schema "users" do
-    field :email, :string,
+    field :email, :string
     field :provider, :string
     field :token, :string
 
