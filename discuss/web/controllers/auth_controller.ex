588da3223b1b522dd0a6c33a050e41a29652ba36
Leland Krych <Leland.Krych@gmail.com>
2017-10-16 22:08:21 -0700

    create signout action for auth_controller and add route

--- discuss/web/controllers/auth_controller.ex
+++ discuss/web/controllers/auth_controller.ex
@@ -11,6 +11,13 @@ defmodule Discuss.AuthController do
       signin(conn, changeset)
     end
 
+    def signout(conn, _params) do
+      conn
+      |> configure_session(drop: true)
+      |> put_flash(:info, "You've logged out")
+      |> redirect(to: topic_path(conn, :index))
+    end
+
     defp signin(conn, changeset) do
       case insert_or_update_user(changeset) do 
         {:ok, user} ->
@@ -33,4 +40,5 @@ defmodule Discuss.AuthController do
           {:ok, user}
       end
     end
+
 end
--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -24,8 +24,10 @@ defmodule Discuss.Router do
   scope "/auth", Discuss do
     pipe_through :browser
 
+    get "/signout", AuthController, :signout
     get "/:provider", AuthController, :request
     get "/:provider/callback", AuthController, :callback
+
 
   end
 
